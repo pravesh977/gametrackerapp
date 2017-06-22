36922408b2444291af4b2fa3e75d31c7e0e1bb0f
pravesh <pravesh_shrestha87@yahoo.com>
2017-06-21 20:58:55 -0400

    adding to heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'games#root'
+  # root 'games#root'
+  root to: 'root#index'
   resources :games
   get 'planning', to: 'games#planning'
   get 'playing', to: 'games#playing'
